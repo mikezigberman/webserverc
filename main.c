f2ce140a54194c556d57c48d2ae8952d937bb49f
Mike Zigberman <mikhail.manager@mail.ru>
2022-02-01 20:33:56 +0100

    webservercommit3

--- main.c
+++ main.c
@@ -106,3 +106,144 @@ int main()
 
     return 0;
 }
+
+void *get_client_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int create_socket(const char *apstrPort)
+{
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    struct addrinfo *p;
+
+    memset(&hints, 0, sizeof(hints));
+
+    // IPv4 or IPv6
+    hints.ai_family   = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags    = AI_PASSIVE;
+
+    int r = getaddrinfo(NULL, apstrPort, &hints, &servinfo);
+    if( r != 0)
+    {
+        fprintf(stderr, "error getaddrinfo()\n");
+        return -1;
+    }
+
+    int sock;
+    int yes;
+    for(p = servinfo; p != NULL; p = p->ai_next)
+    {
+        sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if(sock == -1)
+            continue;
+
+        if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        {
+            fprintf(stderr, "error setsockopt\n");
+            close(sock);
+            freeaddrinfo(servinfo); // all done with this structure
+            return -2;
+        }
+
+        if(bind(sock, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close(sock);
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if(p == NULL)
+    {
+        fprintf(stderr, "failed to find address\n");
+        return -3;
+    }
+
+    if(listen(sock, MAX_CONNECTION) == -1)
+    {
+        fprintf(stderr, "error listen\n");
+        return -4;
+    }
+
+    return sock;
+}
+
+void http_request(int aSock)
+{
+    const int request_buffer_size = 65536;
+    char      request[request_buffer_size];
+
+    int bytes_recvd = recv(aSock, request, request_buffer_size - 1, 0);
+
+    if (bytes_recvd < 0)
+    {
+        fprintf(stderr, "error recv\n");
+        return;
+    }
+    request[bytes_recvd] = '\0';
+
+    printf("request:\n%s\n",request);
+
+    sHTTPHeader req;
+    parse_http_request(request, &req);
+
+    if(req.type == eHTTP_GET)
+    {
+        send_message(aSock, "sensor 1: 10<br> sensor 2: 20<br><a href=\"http://cppprosto.blogspot.com/2017/09/blog-post_23.html\">external</a><br><a href=\"internal\">internal</a>");
+    }
+    else
+    {
+        send_404(aSock);
+    }
+}
+
+void parse_http_request(const char *apstrRequest, sHTTPHeader *apHeader)
+{
+    int  type_length = 0;
+    char type[255]   = {0};
+    int  index = 0;
+
+    apHeader->type = eHTTP_UNKNOWN;
+
+    sscanf(&apstrRequest[index], "%s", type);
+    type_length = strlen(type);
+
+    if(type_length == 3)
+    {
+        if(type[0] == 'G' && type[1] == 'E' && type[2] == 'T')
+            apHeader->type = eHTTP_GET;
+
+        index += type_length + 1;
+        sscanf(&apstrRequest[index], "%s", apHeader->path);
+    }
+}
+
+void send_message(int aSock, const char *apstrMessage)
+{
+    char buffer[65536] = { 0 };
+
+    strcat(buffer, "HTTP/1.1 200 OK\n\n");
+    strcat(buffer, "<h1>");
+    strcat(buffer, apstrMessage);
+    strcat(buffer, "</h1>");
+
+    int len = strlen(buffer);
+    send(aSock, buffer, len, 0);
+}
+
+void send_404(int aSock)
+{
+    const char *buffer = "HTTP/1.1 404 \n\n";
+    int len = strlen(buffer);
+    send(aSock, buffer, len, 0);
+}
